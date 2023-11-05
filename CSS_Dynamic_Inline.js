946ff2c0a2debbc35463ebad945a186dc92ec5be
pixman92 <GOtrustD@verizon.net>
2023-11-05 13:01:39 -0800

    Update CSS_Dynamic_Inline.js

--- CSS_Dynamic_Inline.js
+++ CSS_Dynamic_Inline.js
@@ -1,68 +1,40 @@
-function applyResponsiveStyles(cssText, elementId, innerHTML, newElementId) {
-  let existingElement; // Reference to the existing HTML element
-  let newElement; // Reference to the new HTML element
+let breakpointStyles = {}; // Store styles for specific breakpoints
 
-  /**
-   * Apply styles to the element
-   * @param {HTMLElement} element - The HTML element to apply styles to
-   * @param {string} styles - The CSS styles string to apply
-   */
-  function applyStyles(element, styles) {
-    element.setAttribute('style', styles);
+function determineBreakpoint(screenWidth) {
+  if (screenWidth < 576) {
+    return "sm"; // Below 576px, set as 'sm'
+  } else if (screenWidth < 768) {
+    return "md"; // Below 768px, set as 'md'
+  } else if (screenWidth < 992) {
+    return "lg"; // Below 992px, set as 'lg'
+  } else {
+    return "xl"; // Default to 'xl' for larger screen sizes
   }
+}
 
-  /**
-   * Create the new element with the provided inner HTML
-   */
-  function createNewElement() {
-    newElement = document.createElement('div');
-    newElement.innerHTML = innerHTML;
+function applyStylesToDiv() {
+  const div = document.getElementById("myDiv");
+  const classNames = div.className.split(" ");
+  const screenWidth = window.innerWidth;
+  const breakpoint = determineBreakpoint(screenWidth);
 
-    if (newElementId) {
-      newElement.id = newElementId; // Set the ID of the new element
+  classNames.forEach((className) => {
+    const [breakpointPart, propertyValue] = className.split(":");
+    const [property, value] = propertyValue.split("[");
+    if (breakpointPart === breakpoint) {
+      breakpointStyles[property] = value.slice(0, -1);
     }
-
-    existingElement.appendChild(newElement);
-  }
-
-  /**
-   * Apply the responsive styles and class names to the new element
-   */
-  function applyResponsiveStyles() {
-    let styles = ''; // CSS styles string
-
-    // Find all class names in the CSS text
-    const classRegex = /class:(.*?)\{(.*?)\}/g;
-    const classMatches = cssText.matchAll(classRegex);
-
-    // Check if any class name contains a '$'
-    for (const classMatch of classMatches) {
-      const [, classNames, properties] = classMatch;
-      if (classNames.includes('$')) {
-        const regex = /([a-z]+):(.*?)(?:\[(.*?)\]|;)/g;
-        const matches = properties.matchAll(regex);
-        
-        for (const match of matches) {
-          const [, media, property, value] = match;
-          if (media === 'all' || media === currentMedia) {
-            styles += `${property}:${value};`;
-          }
-        }
+    if (breakpointPart === "all") {
+      if (!breakpointStyles[property]) {
+        breakpointStyles[property] = value.slice(0, -1);
       }
     }
+  });
 
-    applyStyles(newElement, styles);
+  for (const property in breakpointStyles) {
+    div.style[property] = breakpointStyles[property];
   }
-
-  // Event listener for window resize
-  window.addEventListener('resize', applyResponsiveStyles);
-
-  // Get the reference to the existing element
-  existingElement = document.getElementById(elementId);
-
-  // Create the new element once
-  createNewElement();
-
-  // Initial style application
-  applyResponsiveStyles();
 }
+
+window.addEventListener("resize", applyStylesToDiv);
+applyStylesToDiv();

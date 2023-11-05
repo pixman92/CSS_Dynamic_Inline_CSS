e1ced3ace7bc9c209b76fb86e81ddd4fa3913860
pixman92 <GOtrustD@verizon.net>
2023-11-05 12:00:23 -0800

    Update CSS_Dynamic_Inline.js - added "all" feature

--- CSS_Dynamic_Inline.js
+++ CSS_Dynamic_Inline.js
@@ -1,149 +1,8 @@
-window.addEventListener("resize", ()=>{
-handleScreenWidthChange()
-});
-window.onload = function () {
-  // Attach the resize event listener
-
-    if(printIfTrue){
-      console.log(determineBreakpoint(window.innerWidth));
-      // debugger;
-    }
-  // Initial execution to apply the CSS for the current screen width
-  handleScreenWidthChange();
-  }
-
-
-
-// };
-
-let printIfTrue = false;
-
-function changePrintFunction(trueOrFalse){
-  if(trueOrFalse){
-    printIfTrue=true;
-  }else{
-    printIfTrue=false;
-  }
-}
-
-
-// Function to determine the breakpoint based on screen width
-function determineBreakpoint(screenWidth) {
-  if (screenWidth < 480) {
-    return "sm"; // Below 480px, set as 'sm'
-  } else if (screenWidth < 770) {
-    return "md"; // Below 770px, set as 'md'
-  } else if (screenWidth < 990) {
-    return "lg"; // Below 990px, set as 'lg'
-  } else if (screenWidth < 1240) {
-    return "xl"; // Below 1240px, set as 'xl'
-  } else {
-    return "xl"; // Default to 'xl' for larger screen sizes
-  }
-}
-
-// Function to extract CSS attributes and values from class names
-function extractStylesFromClassName(className) {
-  const regex = /(sm|md|lg|xl):([^:\[]+)\[([^\]]+)\]/g;
-  const styles = [];
-  const matches = [...className.matchAll(regex)];
-  for (const match of matches) {
-    const breakpoint = match[1];
-    const attribute = match[2];
-    const value = match[3];
-    styles.push({ breakpoint, attribute, value });
-  }
-  return styles;
-}
-
-// Function to extract classes from class names
-function extractClassesFromClassName(className) {
-  const regex = /(sm|md|lg|xl):\[(.*?)\]/g;
-  const classes = [];
-  const matches = [...className.matchAll(regex)];
-  for (const match of matches) {
-    const breakpoint = match[1];
-    const classString = match[2];
-    classes.push({ breakpoint, classString });
-  }
-  return classes;
-}
-
-// Function to compile the CSS data for each HTML element
-function compileCSSData() {
-  const elements = document.getElementsByClassName("$");
-  const cssData = [];
-  for (let i = 0; i < elements.length; i++) {
-// const dynamicAttributes = ['sm:margin[40px]', 'sm:font-size[40px]', 'sm:color[blue]', 'md:color[red]', 'md:margin[20px]', 'lg:margin[10px]', 'xl:margin[5px]'];
-// const elementId = 'target';
-// const innerHTML = '<h1>Hello, world!</h1>';
-// const dynamicAttributes = ['sm:margin[40px]', 'sm:font-size[40px]', 'sm:color[blue]', 'md:color[red]', 'md:margin[20px]', 'lg:margin[10px]', 'xl:margin[5px]'];
-// const elementId = 'target';
-// const innerHTML = '<h1>Hello, world!</h1>';
-    const element = elements[i];
-    const className = element.className;
-    const styles = extractStylesFromClassName(className);
-    const classes = extractClassesFromClassName(className);
-    cssData.push({ element, styles, classes });
-  }
-  return cssData;
-}
-
-// Function to apply CSS attributes and values to the element
-function applyCSSStyles(element, styles, breakpoint) {
-  // Reset the inline styles of the element
-  element.style.cssText = "";
-
-  // Apply the matching CSS attributes and values to the element
-  for (const { breakpoint: bp, attribute, value } of styles) {
-    // Check if the breakpoint matches the current breakpoint or is a '$'
-    if (bp === breakpoint || bp === "$") {
-      if (bp === breakpoint) {
-        element.style[attribute] = value;
-      } else {
-        element.style[attribute] = "";
-      }
-    }
-  }
-}
-// const dynamicAttributes = ['sm:margin[40px]', 'sm:font-size[40px]', 'sm:color[blue]', 'md:color[red]', 'md:margin[20px]', 'lg:margin[10px]', 'xl:margin[5px]'];
-// const elementId = 'target';
-// const innerHTML = '<h1>Hello, world!</h1>';
-
-// Function to apply classes to the element
-function applyClasses(element, classes, breakpoint) {
-  // Iterate over the classes and apply them based on the breakpoint
-  for (const { breakpoint: bp, classString } of classes) {
-    if (bp === breakpoint) {
-      element.classList.add(classString);
-    } else {
-// const dynamicAttributes = ['sm:margin[40px]', 'sm:font-size[40px]', 'sm:color[blue]', 'md:color[red]', 'md:margin[20px]', 'lg:margin[10px]', 'xl:margin[5px]'];
-// const elementId = 'target';
-// const innerHTML = '<h1>Hello, world!</h1>';
-      element.classList.remove(classString);
-    }
-  }
-}
-
-// Function to handle screen width changes and apply corresponding CSS data
-function handleScreenWidthChange() {
-  const screenWidth = window.innerWidth;
-  const cssData = compileCSSData();
-  const currentBreakpoint = determineBreakpoint(screenWidth);
-  for (const { element, styles, classes } of cssData) {
-    applyCSSStyles(element, styles, currentBreakpoint);
-    applyClasses(element, classes, currentBreakpoint);
-  }
-}
-
-// ========================================
-
-// Function to create a new HTML element with dynamic
-
 function applyResponsiveStyles(cssText, elementId, innerHTML, newElementId) {
   let currentMedia = 'xl'; // Default media breakpoint
   let existingElement; // Reference to the existing HTML element
   let newElement; // Reference to the new HTML element
+  let allStyles = {}; // Store "all" CSS properties and values
 
   /**
    * Apply styles to the element
@@ -200,7 +59,12 @@ function applyResponsiveStyles(cssText, elementId, innerHTML, newElementId) {
     for (const match of matches) {
       const [, media, property, value] = match;
       if (media === currentMedia && property && value) {
-        styles += `${property}:${value};`;
+        if (media === 'all') {
+          allStyles[property] = value; // Store "all" styles
+        } else {
+          allStyles[property] = ''; // Clear "all" styles
+          styles += `${property}:${value};`;
+        }
       }
     }
 
@@ -212,6 +76,11 @@ function applyResponsiveStyles(cssText, elementId, innerHTML, newElementId) {
       if (className) {
         classList += `${className} `;
       }
+    }
+
+    // Apply "all" styles first
+    for (const property in allStyles) {
+      styles += `${property}:${allStyles[property]};`;
     }
 
     applyStyles(newElement, styles);
@@ -233,17 +102,3 @@ function applyResponsiveStyles(cssText, elementId, innerHTML, newElementId) {
   // Initial style and class application
   applyResponsiveStyles();
 }
-// ====================
-// simple function to change CSS on the fly
-function changeCSS(selector, attribute, value) {
-  let selected = document.querySelector(selector);
-  selected.style[attribute] = value;
-}
-
-
-
-// function applyResponsiveStyles(cssText, elementId, innerHTML, newElementId) {
-// cssText - text to apply to CSS style
-// elementID - ID of HTML element, to attach new Element
-// innerHTML - html to add build new Element
-// newElementId - ID to add to new HTML element - can be empty string
